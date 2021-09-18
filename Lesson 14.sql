f60cabae6e65f564a32a55144e641bc5320495d0
Hasanov Asadullo Abduvosievich <88389289+Lion-Asad@users.noreply.github.com>
2021-09-18 18:32:03 +0500

    Update Lesson 14.sql

--- Lesson 14.sql
+++ Lesson 14.sql
@@ -22,6 +22,6 @@ create table Accounts (
 	Created_At datetime not null,
 	Updated_At datetime not null,
 
-	AccountsClient_Id int references Clients (Id) not null,
-	AccoutnsCurrency_Id int references Currencies (Id) not null
+	Accounts.Client_Id int references Clients (Id) not null,
+	Accoutns.Currency_Id int references Currencies (Id) not null
 );
